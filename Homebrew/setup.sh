734113432022d58cb4e3ee8ba31f7719fd2943c4
Yohei Ema <24579635+meihei3@users.noreply.github.com>
2021-08-28 01:35:20 +0900

    Update setup.sh

--- Homebrew/setup.sh
+++ Homebrew/setup.sh
@@ -18,4 +18,6 @@ if [ ! -e $FILENAME ]; then
 fi
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
 brew bundle --global
